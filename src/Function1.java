bcfde36c03ed2c446ee4e40be557fbcc5e60f67c
xuchunfa <731227680@qq.com>
2020-01-30 15:15:51 +0800

    xuchunfa finished function1

---
+++ src/Function1.java
@@ -0,0 +1,10 @@
+/**
+ * @description:
+ * @author: Xu chunfa
+ * @create: 2020-01-30 15:13
+ **/
+public class Function1 {
+    public static void main(String[] args){
+        System.out.println("xuchunfa is charge of Function1");
+    }
+}
