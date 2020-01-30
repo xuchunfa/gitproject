c7dae15649ae3891dd7615966bb010afbcd93382
xuchunfa <731227680@qq.com>
2020-01-30 14:20:30 +0800

    这是一个由xuchunfa和lisa负责的项目

---
+++ src/Test.java
@@ -0,0 +1,10 @@
+/**
+ * @description:
+ * @author: Xu chunfa
+ * @create: 2020-01-30 13:55
+ **/
+public class Test {
+    public static void main(String[] args){
+        System.out.println("hello github");
+    }
+}
