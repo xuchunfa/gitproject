9d4fd32750bb7ac4e53d8c1395e81dfca0891611
xuchunfa <731227680@qq.com>
2020-01-30 18:01:43 +0800

    tom finished TomFunction

---
+++ src/TomFunction.java
@@ -0,0 +1,10 @@
+/**
+ * @description:
+ * @author: Xu chunfa
+ * @create: 2020-01-30 18:00
+ **/
+public class TomFunction {
+    public static void main(String[] args){
+        System.out.println("Tom is charge of TomFunction");
+    }
+}
